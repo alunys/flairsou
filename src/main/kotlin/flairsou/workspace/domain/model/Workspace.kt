4f306f5497ce6cb3e080699a0004c4f976877fd5
Parmentier Raphael <alunys@users.noreply.github.com>
2019-11-30 02:17:42 +0100

    Test (#5)
    * Create gradle.yml
    
    * test WorkspaceRepository
    
    * remove github action

--- src/main/kotlin/flairsou/workspace/domain/model/Workspace.kt
+++ src/main/kotlin/flairsou/workspace/domain/model/Workspace.kt
@@ -3,8 +3,9 @@ package flairsou.workspace.domain.model
 data class Workspace(val uuid: String) {
 
 
+
     companion object{
-        val workspaceName=""
+        val workspaceName="flairsou"
 
     }
 
